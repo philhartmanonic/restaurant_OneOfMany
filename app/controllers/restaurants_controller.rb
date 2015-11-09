01dbb9a680b054a75d1325ca369acaafbc727b2d
Mark Ducommun <ducommun.mark@gmail.com>
2015-11-09 09:28:15 -0600

    starting the create action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,19 @@ class RestaurantsController < ApplicationController
   def new
     @restaurant = Restaurant.new
   end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(
+      :name,
+      :description,
+      :full_address,
+      :phone_number
+    )
+  end
 end
